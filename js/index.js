9e67f3891c44262ac7e133a0eea5799bfee3f9fc
machineManSvg <sefiksevgener@gmail.com>
2026-02-07 22:51:03 +0200

    button

--- js/index.js
+++ js/index.js
@@ -7,9 +7,13 @@ window.alert("Alert 2!!!");
 */
 document.getElementById("myH1").textContent = "Hello, World!";
 document.getElementById("myP").textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-
+document.getElementById("myBtn").addEventListener("click", function() {
+    alert("Button clicked!");
+});
 
 const x = 10;
 const y = 20;
 const sum = x + y;
+
+document.getElementById("myH1").textContent = `The sum of ${x} and ${y} is: ${sum}`;
 
