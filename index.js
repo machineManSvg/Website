8cec4c4b7a86e4cc8b6330607a5e908b27b23231
machineManSvg <sefiksevgener@gmail.com>
2026-02-07 22:05:53 +0200

    text

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ console.log("This is a simple Node.js application.");
 
 window.alert("Alert!!!");
 window.alert("Alert 2!!!");
-
+*/
 document.getElementById("myH1").textContent = "Hello, World!";
 document.getElementById("myP").textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-*/
+
 
 const x = 10;
 const y = 20;
